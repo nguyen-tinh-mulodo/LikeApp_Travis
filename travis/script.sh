09c6b438fd1dabc65049868d9f17e2dd52e3b5c5
nguyen-tinh-mulodo <nguyen.tinh@mulodo.com>
2013-07-29 09:14:42 +0700

    Update script.sh

--- travis/script.sh
+++ travis/script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-xctool/xctool.sh -project LikeApp.xcodeproj -scheme travis build
+xctool -project LikeApp.xcodeproj -scheme travis build
 xctool -project LikeApp.xcodeproj -scheme travis build test
 #
 
